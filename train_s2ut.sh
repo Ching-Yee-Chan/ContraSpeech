ba44639ada4a14665bc37027171d812fdbc92370
Ching-Yee-Chan <fkzxy4213@qq.com>
2023-05-22 20:10:09 +0800

    s2ut train

--- train_s2ut.sh
+++ train_s2ut.sh
@@ -1,8 +1,7 @@
 DATA_ROOT=/home/zhaojiankun/zhaojiankun_space/TranSpeech/format_s2st
 MODEL_DIR=save_ckpt
 
-
-CUDA_VISIBLE_DEVICES=4,5,6,7 PYTHONPATH=. fairseq-train $DATA_ROOT \
+CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=. fairseq-train $DATA_ROOT \
   --config-yaml config.yaml \
   --task speech_to_speech_fasttranslate --target-is-code --target-code-size 1000 --vocoder code_hifigan  \
   --criterion nar_speech_to_unit --label-smoothing 0.2 \
@@ -10,8 +9,8 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 PYTHONPATH=. fairseq-train $DATA_ROOT \
   --dropout 0.1 --attention-dropout 0.1 --relu-dropout 0.1 \
   --train-subset train --valid-subset dev \
   --save-dir $MODEL_DIR  --tensorboard-logdir $MODEL_DIR \
-  --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-7 --warmup-updates 1000 \
+  --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-7 --warmup-updates 10000 \
   --optimizer adam --adam-betas "(0.9,0.98)" --clip-norm 10.0 \
-  --max-update 20000 --max-tokens 20000 --max-target-positions 3000 --update-freq 4 \
+  --max-update 200000 --max-tokens 20000 --max-target-positions 3000 --update-freq 4 \
   --seed 1 --fp16 --num-workers 8 \
   --user-dir research/  --attn-type espnet --pos-enc-type rel_pos 
