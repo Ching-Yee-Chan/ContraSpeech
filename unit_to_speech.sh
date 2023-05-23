8636301f19f45feee9e8637275179fd34f71b77a
Ching-Yee-Chan <fkzxy4213@qq.com>
2023-05-23 21:42:00 +0800

    u2st inference

--- unit_to_speech.sh
+++ unit_to_speech.sh
@@ -1,10 +1,10 @@
-RESULTS_PATH=/home/zhaojiankun/zhaojiankun_space/TranSpeech/tmp
+RESULTS_PATH=/home/zhaojiankun/zhaojiankun_space/TranSpeech/res_output
 GEN_SUBSET=test
 VOCODER_CKPT=/home/zhaojiankun/zhaojiankun_space/TranSpeech/ckpt/vocoder/g_00500000
 VOCODER_CFG=/home/zhaojiankun/zhaojiankun_space/TranSpeech/ckpt/vocoder/config.json
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 PYTHONPATH=. python examples/speech_to_speech/generate_waveform_from_code.py \
+CUDA_VISIBLE_DEVICES=0,1,2,5 PYTHONPATH=. python examples/speech_to_speech/generate_waveform_from_code.py \
   --in-code-file ${RESULTS_PATH}/generate-${GEN_SUBSET}.unit \
   --vocoder $VOCODER_CKPT --vocoder-cfg $VOCODER_CFG \
-  --results-path ${RESULTS_PATH} 
-  # --results-path ${RESULTS_PATH} --dur-prediction
+  --results-path ${RESULTS_PATH} --dur-prediction
+  # --results-path ${RESULTS_PATH} 
