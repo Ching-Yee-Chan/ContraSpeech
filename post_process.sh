fd4bcbabf8bcce054cd076d6e31d6aa221286deb
Ching-Yee-Chan <fkzxy4213@qq.com>
2023-05-23 21:41:47 +0800

    s2ut finetune and inference

--- post_process.sh
+++ post_process.sh
@@ -1,17 +1,5 @@
-RESULTS_PATH=/llm/nankai/zhaojiankun_space/TranSpeech/hubert_output
-DATA_DIR=/llm/nankai/zhaojiankun_space/cvss_c/ch/format_data
-
-SUBSET_NAME=train
-CUDA_VISIBLE_DEVICES=1 PYTHONPATH=. python examples/speech_to_speech/preprocessing/prep_sn_output_data.py \
-    --in-unit $RESULTS_PATH/hypo.units \
-    --in-audio $DATA_DIR/$SUBSET_NAME.tsv \
-    --output-root ${RESULTS_PATH}
-
-SUBSET_NAME=test
-CUDA_VISIBLE_DEVICES=1 PYTHONPATH=. python examples/speech_to_speech/preprocessing/prep_sn_output_data.py \
-    --in-unit $RESULTS_PATH/hypo.units \
-    --in-audio $DATA_DIR/$SUBSET_NAME.tsv \
-    --output-root ${RESULTS_PATH}
+RESULTS_PATH=/llm/nankai/zhaojiankun_space/TranSpeech/output/hubert_output
+DATA_DIR=/llm/nankai/zhaojiankun_space/cvss_c/ru/format_data
 
 SUBSET_NAME=dev
 CUDA_VISIBLE_DEVICES=1 PYTHONPATH=. python examples/speech_to_speech/preprocessing/prep_sn_output_data.py \
--- train_s2ut.sh
+++ train_s2ut.sh
@@ -1,7 +1,9 @@
-DATA_ROOT=/home/zhaojiankun/zhaojiankun_space/TranSpeech/format_s2st
-MODEL_DIR=save_ckpt
+DATA_ROOT=/home/zhaojiankun/zhaojiankun_space/TranSpeech/output/format_s2st
+MODEL_DIR=/home/zhaojiankun/zhaojiankun_space/TranSpeech/output/s2ut_ckpt
+LOG_DIR=/home/zhaojiankun/zhaojiankun_space/TranSpeech/output/log
+DATE_TIME=$(date +%m%d%H%M)
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=. fairseq-train $DATA_ROOT \
+CUDA_VISIBLE_DEVICES=1,2,3,7 PYTHONPATH=. nohup fairseq-train $DATA_ROOT \
   --config-yaml config.yaml \
   --task speech_to_speech_fasttranslate --target-is-code --target-code-size 1000 --vocoder code_hifigan  \
   --criterion nar_speech_to_unit --label-smoothing 0.2 \
@@ -11,6 +13,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 PYTHONPATH=. fairseq-train $DATA_ROOT \
   --save-dir $MODEL_DIR  --tensorboard-logdir $MODEL_DIR \
   --lr 0.0005 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-7 --warmup-updates 10000 \
   --optimizer adam --adam-betas "(0.9,0.98)" --clip-norm 10.0 \
-  --max-update 200000 --max-tokens 20000 --max-target-positions 3000 --update-freq 4 \
+  --max-update 400000 --max-tokens 20000 --max-target-positions 3000 --update-freq 4 \
   --seed 1 --fp16 --num-workers 8 \
-  --user-dir research/  --attn-type espnet --pos-enc-type rel_pos 
+  --user-dir research/  --attn-type espnet --pos-enc-type rel_pos \
+  > ${LOG_DIR}/log_s2ut_train_${DATE_TIME}.txt &
